10673a0a7692eccb43201ed6a82afdffcba56917
Enrique Martinez <emartinezc@gmail.com>
2021-02-16 09:23:53 -0600

    remove config folder

--- src/MusicStoreNetCore/Infrastructure/MusicStoreContext.cs
+++ src/MusicStoreNetCore/Infrastructure/MusicStoreContext.cs
@@ -1,6 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using MusicStoreNetCore.Domain;
-using System.Reflection;
 
 namespace MusicStoreNetCore.Infrastructure
 {
@@ -10,7 +9,7 @@ namespace MusicStoreNetCore.Infrastructure
         {
         }
 
-        public  DbSet<Album> Albums { get; set; }
+        public DbSet<Album> Albums { get; set; }
         public DbSet<Artist> Artists { get; set; }
         public DbSet<Genre> Genres { get; set; }
         public DbSet<Order> Orders { get; set; }
@@ -19,8 +18,27 @@ namespace MusicStoreNetCore.Infrastructure
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
-            base.OnModelCreating(builder);
-            builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+            builder.Entity<Album>()
+                .ToTable("Album")
+                .Property(t => t.Price)
+                .IsRequired()
+                .HasColumnType("decimal(18,2)");
+
+            builder.Entity<Artist>().ToTable("Album");
+
+            builder.Entity<Genre>().ToTable("Genre");
+
+            builder.Entity<Order>()
+               .ToTable("Order")
+               .Property(t => t.Total)
+               .HasColumnType("decimal(18,2)");
+
+            builder.Entity<OrderDetail>()
+               .ToTable("OrderDetail")
+               .Property(t => t.UnitPrice)
+               .HasColumnType("decimal(18,2)");
+
+            builder.Entity<CartItem>().ToTable("CartItem");
         }
     }
 }
